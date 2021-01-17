5d821a76bdb08199caac402383db74d658728fbf
TorstenAndersson <65869245+TorstenAndersson@users.noreply.github.com>
2021-01-17 21:31:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 //const PORT = process.env.PORT
 
+/*
 express()
   .get('/', (req, res) => res.render('products.json'))
   .listen(PORT, () => console.log(`Listening on ${ process.env.PORT }`))
+*/
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
