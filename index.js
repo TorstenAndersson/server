1f9bf61be6c9b0dda6bbaeba9116e483d141028a
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-01-20 21:51:11 +0100

    maybe

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ for (var i in products.products) {
   if (products.products[i].type = "sudd") {sudd.push(products.products[i])};
   if (products.products[i].type = "merchandise") {merchandise.push(products.products[i])};
   if (products.products[i].ondisplay = "true") {onDisplay.push(products.products[i])};
-  individualItems += '"' + (products.products[i].name.replace(" ", "") + '":' + JSON.stringify(products.products[i]) + '');
+  individualItems += '"' + (products.products[i].name.replace(" ", "") + '":' + JSON.stringify(products.products[i]) + ',');
   //individualItems.push(products.products[i].name);
 }
 
 individualItems = individualItems.slice(0, -1);
-individualItems += "}]";
+individualItems += "]}";
 
 express()
   //.get('/', (req, res) => res.send(JSON.parse(individualItems).items.SuddJohan))
