9ae6786efeaa7a774f919ab40bf5548868c512d1
TorstenAndersson <65869245+TorstenAndersson@users.noreply.github.com>
2021-01-20 21:26:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ individualItems = individualItems.slice(0, -1);
 individualItems += "}";
 
 express()
-  .get('/', (req, res) => res.send(JSON.parse(individualItems).items.SuddJohan))
+  //.get('/', (req, res) => res.send(JSON.parse(individualItems).items.SuddJohan))
+  .get('/', (req, res) => res.send(individualItems.items))
   .get('/products', (req, res) => res.json(products))
   .listen(port, () => console.log(`Listening on ${port}`));
