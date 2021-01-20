a31891091771e29053140755046653c89f205962
TorstenAndersson <65869245+TorstenAndersson@users.noreply.github.com>
2021-01-20 20:01:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var onDisplay = [];
 var individualItems = [];
 
 for (var i in products.products) {
-  if (products.products[i].type = "sudd") {sudd.push(products.products[i])};
-  if (products.products[i].type = "merchandise") {merchandise.push(products.products[i])};
-  if (products.products[i].ondisplay = "true") {onDisplay.push(products.products[i])};
+  if (products.products[i].type = "sudd") {sudd.push(JSON.stringifyproducts.products[i]))};
+  if (products.products[i].type = "merchandise") {merchandise.push(JSON.stringifyproducts.products[i]))};
+  if (products.products[i].ondisplay = "true") {onDisplay.push(JSON.stringify(products.products[i]))};
   individualItems.push('{"' + products.products[i].name + '":' + JSON.stringify(products.products[i]) + '}');
   //individualItems.push(products.products[i].name);
 }
