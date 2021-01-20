2436189a6d89fceb691eda417f3fc7c526bba36c
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-01-20 21:56:24 +0100

    maybe

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ individualItems += "]}";
 
 express()
   //.get('/', (req, res) => res.send(JSON.parse(individualItems).items.SuddJohan))
-  .get('/', (req, res) => res.send(individualItems))
+  .get('/', (req, res) => res.send(JSON.parse(individualItems)))
   .get('/products', (req, res) => res.json(products))
   .listen(port, () => console.log(`Listening on ${port}`));
