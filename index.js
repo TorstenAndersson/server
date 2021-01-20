078644b99b4d37fe5ff52a2aa01f72ef1bee3f42
TorstenAndersson <65869245+TorstenAndersson@users.noreply.github.com>
2021-01-20 20:18:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ for (var i in products.products) {
 }
 
 express()
-  .get('/', (req, res) => res.send(individualItems.Sudd Johan))
+  .get('/', (req, res) => res.send(individualItems.SuddJohan))
   .get('/products', (req, res) => res.json(products))
   .listen(port, () => console.log(`Listening on ${port}`));
