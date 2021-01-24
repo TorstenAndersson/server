42a9efc88779d92c74ac158e7c36d0a1851149be
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-01-24 23:09:39 +0100

    maybe

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors")
 const port = process.env.PORT;
 const products = JSON.parse('{"products": [{"name":"Sudd Johan","description":"Bara gamla goda Johan.","price":{"discount": {"price":"10,99 kr","reason":"Fick vibbarna"},"original":"14, 99 kr"},"img":"imgs/Sudd/Sudd Johan.png","link":"sudd/sudd-johan","type":"sudd","onDisplay":"true"}, {"name":"Johans Hoodie","description":"fyfan va varmt","price":{"discount": {"price":"149,99 kr","reason":"PÅSKREA"},"original":"499,99 kr"},"img":"imgs/Johans Hoodie/Johans Hoodie White Front.jpg","link":"merchandise/johans-hoodie","type":"merchandise","onDisplay":"true"}, {"name":"Johans Flip-Flops","description":"LFIPÅPT FLOP","price":{"discount": {"price":"1000,99 kr","reason":"PÅSKREA"},"original":"349, 99 kr"},"img":"imgs/Johans Flip-Flops/Johans Flip-Flops Medium Above.png","link":"merchandise/johans-flip-flops","type":"merchandise","onDisplay":"true"}, {"name":"Johans Mobilskal","description":"Skydda din mobil!","price":{"original":"149,99 kr"},"img":"imgs/Johans Mobilskal/Johans Mobilskal iPhone 12 Pro Max.png","link":"merchandise/johans-mobilskal","type":"merchandise","onDisplay":"true"}]}');
 
@@ -26,28 +27,19 @@ individualItems += "}}";
 
 express()
 
-  .get("/", function(req, res) {
+  .get("/", cors(), function(req, res) {
     res.send("The Official Server of Johanssudd");
   })
 
-  .get("/products", function(req, res) {
-    res.append("Access-Control-Allow-Origin", "https://xenodochial-varahamihira-6786dc.netlify.app/");
-    res.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.append("Access-Control-Allow-Headers", "Authentication, Content-Type");
+  .get("/products", cors(), function(req, res) {
     res.json(JSON.parse(eval(req.query.wanted)));
   })
 
-  .get("/product", function(req, res) {
-    res.append("Access-Control-Allow-Origin", "https://xenodochial-varahamihira-6786dc.netlify.app/");
-    res.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.append("Access-Control-Allow-Headers", "Authentication, Content-Type");
+  .get("/product", cors(), function(req, res) {
     res.json(JSON.parse(individualItems).items[req.query.wanted])
   })
 
-  .get("/sudd", function(req, res) {
-    res.append("Access-Control-Allow-Origin", "https://xenodochial-varahamihira-6786dc.netlify.app/");
-    res.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.append("Access-Control-Allow-Headers", "Authentication, Content-Type");
+  .get("/sudd", cors(), function(req, res) {
     res.json(JSON.parse(sudd));
   })
 
