e1f5df5edfd6735017a42f2479c24228bf8c6180
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-01-25 22:49:31 +0100

    maybe

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT;
 const products = JSON.parse('{"products": [{"name":"Sudd Johan","description":"Bara gamla goda Johan.","price":{"discount": {"price":"10,99 kr","reason":"Fick vibbarna"},"original":"14, 99 kr"},"img":"imgs/Sudd/Sudd Johan.png","link":"sudd/sudd-johan","type":"sudd","onDisplay":"true"}, {"name":"Johans Hoodie","description":"fyfan va varmt","price":{"discount": {"price":"149,99 kr","reason":"PÅSKREA"},"original":"499,99 kr"},"img":"imgs/Johans Hoodie/Johans Hoodie White Front.jpg","link":"merchandise/johans-hoodie","type":"merchandise","onDisplay":"true"}, {"name":"Johans Flip-Flops","description":"LFIPÅPT FLOP","price":{"discount": {"price":"1000,99 kr","reason":"PÅSKREA"},"original":"349, 99 kr"},"img":"imgs/Johans Flip-Flops/Johans Flip-Flops Medium Above.png","link":"merchandise/johans-flip-flops","type":"merchandise","onDisplay":"true"}, {"name":"Johans Mobilskal","description":"Skydda din mobil!","price":{"original":"149,99 kr"},"img":"imgs/Johans Mobilskal/Johans Mobilskal iPhone 12 Pro Max.png","link":"merchandise/johans-mobilskal","type":"merchandise","onDisplay":"true"}]}');
 
 var sudd = '{"items":[{';
-var merchandise = '{"items":[{';
+var merchandise = '{"items":[';
 var onDisplay = '{"items":[{';
 var individualItems = '{"items":{';
 
@@ -19,7 +19,7 @@ for (var i in products.products) {
 sudd = sudd.slice(0, -1);
 sudd += "}]}";
 merchandise = merchandise.slice(0, -1);
-merchandise += "}]}";
+merchandise += "]}";
 onDisplay = onDisplay.slice(0, -1);
 onDisplay += "}]}";
 individualItems = individualItems.slice(0, -1);
